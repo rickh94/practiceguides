04395aecdc261045825f95a6c32e16b253670795
Rick Henry <rickhenry@rickhenry.dev>
2023-10-11 22:34:40 -0400

    add default from email
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- app/studiowiki/settings.py
+++ app/studiowiki/settings.py
@@ -145,12 +145,14 @@ LOGIN_REDIRECT_URL = "/"
 
 if DEBUG:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    DEFAULT_FROM_EMAIL = "test@example.com"
 else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = os.getenv("EMAIL_HOST")
     EMAIL_PORT = os.getenv("EMAIL_PORT")
     EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+    DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
     EMAIL_USE_TLS = True
 
 
